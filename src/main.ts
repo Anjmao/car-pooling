b48cbd7b82d500421b1b5578fff4d3a0bb1d1047
varnastadeus <varnastadeus@gmail.com>
2016-07-29 10:49:13 +0300

    add main steps

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+class Main {
+    calculate() {
+
+        //0.   import data
+
+        //1:   sort by longtitude
+        //1.1: group driver with possible passengers 
+
+        //2:   find possible matches for each driver 
+
+        //3:   decide 
+        //3.1: match driver with best passenger, remove passenger from future matches
+    }
+}
