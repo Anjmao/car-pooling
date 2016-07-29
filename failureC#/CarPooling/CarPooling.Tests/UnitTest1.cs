981699277f425261c46473a865f1a52fde03c0fe
Andzej <andzej.mac@gmail.com>
2016-07-29 14:50:38 +0300

    tests project

---
+++ failureC#/CarPooling/CarPooling.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarPooling.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
